be94a7313e95076643e3bf2b300c8ff4542369f2
code-walker-23 <chhabravinay549@gmail.com>
2025-04-25 19:33:40 +0530

    Added SRP

--- src/Lesson1_SingleResponsibilityPrinciple/TakeHomeTask/SchoolManagerSolutionSRP.java
+++ src/Lesson1_SingleResponsibilityPrinciple/TakeHomeTask/SchoolManagerSolutionSRP.java
@@ -1,3 +1,9 @@
+/*
+* ✅ Rule of Thumb:
+Ask yourself: "If one of these methods changes, is it likely the others need to change too?"
+If yes → they are related.
+If no → they should probably be separated.
+* */
 package Lesson1_SingleResponsibilityPrinciple.TakeHomeTask;
 class Student {
     // Student-related properties and methods
